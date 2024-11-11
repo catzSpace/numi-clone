0eb53c0a3fc00b0e87817b257162bd84154738c3
catzspace <o8catzspace8o@gmail.com>
2024-11-11 17:27:45 -0500

    test

--- src/fetch/fetch.js
+++ src/fetch/fetch.js
@@ -4,27 +4,34 @@ const fs = require('fs')
 //TODO 
 /* funcion para extraer la base y el target de un archivo txt */
 
-let base = "USD"
-let target = "BTC"
+let base = ""
+let target = ""
 let response
+
+fs.readFile('./src/fetch/input.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error al leer el archivo:', err);
+    } else {
+        console.log('Contenido del archivo:', data);
+        base = data.slice(0, 3);
+    }
+});
+
+let b = "USD"
 
 
 // const urls = `https://exchange-rates.abstractapi.com/v1/live/?api_key=f768b79aa08e48daade82fb9f2c8fab7&base=${base}&target=${target}`
-const url = `https://exchange-rates.abstractapi.com/v1/live/?api_key=f768b79aa08e48daade82fb9f2c8fab7&base=${base}`
+const url = `https://exchange-rates.abstractapi.com/v1/live/?api_key=&base=${b}`
 
 
 async function getData() {
 
   const res = await fetch(url, {  
     method: "GET",
-    withCredentials: true,
-    headers: {
-      "X-Auth-Token": "",
-      "Content-Type": "application/json"
-    }
   })
 
   const data = await res.json()
+  console.log(data)
 
   // sobreescribir archivo json con los nuevos datos
   const jsonData = JSON.stringify(data, null, 2);
